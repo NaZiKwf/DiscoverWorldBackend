0709a3dc873a974a15ea0c7ef3435d3883d2c3ea
NaZiKwf <nazar.akolovich@gmail.com>
2023-05-12 17:02:15 +0300

    Initial commit

---
+++ src/main/java/ua/nure/akolovych/discoverworld/DiscoverWorldApplication.java
@@ -0,0 +1,15 @@
+package ua.nure.akolovych.discoverworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class DiscoverWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverWorldApplication.class, args);
+	}
+
+}
